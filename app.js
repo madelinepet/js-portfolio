f404ebb45c0273c9a31244416d535e2184e6190c
MADELINE PETERS <madelinepet@hotmail.com>
2019-01-09 10:49:37 -0800

    silly jquery game added.

---
+++ app.js
@@ -0,0 +1,25 @@
+
+
+// instead set css here
+
+$("#projlink").click(function() {
+    $("#projects").show();
+    $("#about").hide();
+    $("#game").hide();
+  })
+
+$("#home").click(function(){
+    $("#about").show();
+    $("#projects").hide();
+    $("#game").hide();
+  })
+
+$("#game-link").click(function(){
+    $("#about").hide();
+    $("#projects").hide();
+    $("#game").show();
+})
+
+$(".frogicorn").click(function(){
+    $(this).slideUp();
+})
